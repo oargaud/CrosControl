963620b67dffaf73327f6f72201de6a2d413ff4b
olivierargaud <olivier.argaud@ipilyon.net>
2021-12-30 11:06:16 +0100

    fix temporaire allows "*" CORS

--- src/main/java/com/futark/CrosControl/CrosControlApplication.java
+++ src/main/java/com/futark/CrosControl/CrosControlApplication.java
@@ -17,11 +17,28 @@ public class CrosControlApplication {
 	}
 
 
+//	@Bean
+//	public CorsFilter corsFilter() {
+//		CorsConfiguration corsConfiguration = new CorsConfiguration();
+//		corsConfiguration.setAllowCredentials(false);
+//		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://localhost:8081","http://localhost:8080","93.9.238.159"));
+//		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin","Access-Control-Allow-Origin","Content-Type",
+//				"Accept", "Authorization", "Origin, Accept","X-Requested-With",
+//				"Access-Control-Request-Method","Access-Control-Request-Headers"));
+//		corsConfiguration.setExposedHeaders(Arrays.asList("Origin","Content-Type","Accept","Authorization",
+//				"Access-Control-Allow-Origin","Access-Control-Allow-Credentials"));
+//		corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
+//		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+//		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",corsConfiguration);
+//		return new CorsFilter(urlBasedCorsConfigurationSource);
+//	}
+
 	@Bean
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://localhost:8081"));
+		corsConfiguration.validateAllowCredentials();
+		corsConfiguration.setAllowedOriginPatterns(Arrays.asList("http://localhost:4200","http://localhost:8081","http://localhost:8080","93.9.238.159","*"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin","Access-Control-Allow-Origin","Content-Type",
 				"Accept", "Authorization", "Origin, Accept","X-Requested-With",
 				"Access-Control-Request-Method","Access-Control-Request-Headers"));
